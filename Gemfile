c9d6c7e679cded11541dacbdc1e658f029034db0
Janderson Ferreira <ffjanderson@gmail.com>
2012-03-23 18:59:39 -0300

    testes projetos

--- Gemfile
+++ Gemfile
@@ -41,6 +41,6 @@ group :test do
   # Pretty printed test output
   gem 'turn', :require => false
 end
-
+gem "twitter-bootstrap-rails"
 gem "omniauth-twitter"
 gem "omniauth-facebook"
--- test/unit/projeto_test.rb
+++ test/unit/projeto_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class ProjetoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures :usuarios
+  test "projeto invalido" do
+		projeto = create(:nome=>nil, :user_id=>nil, :descricao=>nil)
+		assert(!projeto.valid?)
+  end
+  
+  test "nome projeto deve existir" do
+		projeto = create(:nome=>nil)
+		assert(!projeto.valid?)
+  end
+  
+  test "descricao projeto deve existir" do
+		projeto = create(:descricao=>nil)
+		assert(!projeto.valid?)
+  end
+  
+  test "usuario projeto deve existir" do
+		projeto = create()
+		projeto.usuario_id = nil
+		projeto.valid?
+		assert(!projeto.valid?)
+  end
+  
+  test "usuario_id nao permit mass assign" do
+		projeto = Projeto.create( :usuario_id=>1,
+        :nome => "Abre te sesamo",
+        :descricao=> "este projeto vai abrir uma grande porta")
+		assert(projeto.usuario_id.nil?)
+  end
+  
+  
+  private
+    def create(options={})
+      p = Projeto.create({
+        :nome => "Abre te sesamo",
+        :descricao=> "este projeto vai abrir uma grande porta"
+        }.merge(options))
+      p.usuario_id = 1
+      return p
+    end
 end
