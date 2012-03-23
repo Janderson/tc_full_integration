be8070aea72b1bb62accf40f45f6f90c5b1c1439
Janderson Ferreira <ffjanderson@gmail.com>
2012-03-23 12:04:48 -0300

    testes adicionados e projetos

--- app/models/projeto.rb
+++ app/models/projeto.rb
@@ -2,6 +2,8 @@ class Projeto < ActiveRecord::Base
 	attr_accessible :nome, :descricao
 	belongs_to :usuario
 	validates :usuario_id, :presence=>true
+	validates :nome, :presence=>true
+	validates :descricao, :presence=>true
 	def to_s
 		nome
 	end
